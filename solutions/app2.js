6ca29df90302cd98c77e81279240ab161a60557c
Hajni83 <maro.hajnalka@gmail.com>
2021-11-11 22:27:09 +0100

    2 task

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,21 @@
+const setCookie = (name, value) => {
+  let now = new Date();
+  now.setTime(now.getTime() + 15 * 60 * 1000);
+  let expires = now.toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}`;
+};
 
+setCookie("viewed", "5");
+setCookie("uid", "354774631237");
+setCookie("ssid", "Bx55OWbHJ0Vt_IGIF");
 
+const cookieHandler = {
+  getAll() {
+    
 
+  },
+  toSessionStorage() {},
+  flush() {},
+};
 
-
-
-// export { cookieHandler };
+export { cookieHandler };
